28d5216f17e3762654e9c59bcf3338a14a2f7a53
rio-vps <testuser01@defomate.net>
2022-01-02 12:56:49 +0900

    メール認証のリンク先を変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
   
   config.action_mailer.delivery_method = :ses
   
-  config.action_mailer.default_url_options = {  :host => 'illustlife.com', :protocol => 'https' }
+  config.action_mailer.default_url_options = {  :host => 'sokuboko.com', :protocol => 'https' }
   
   #メール送受信でエラーがあった場合に、エラー内容を出力する
   config.action_mailer.raise_delivery_errors = true
