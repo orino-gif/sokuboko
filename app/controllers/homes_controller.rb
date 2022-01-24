d631bb8201a88821930e5c433af31a2cd04f4a06
rio <xr274375@fc5.so-net.ne.jp>
2022-01-24 17:09:55 +0900

    利用料金のページを追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,4 +2,12 @@ class HomesController < ApplicationController
   def index
     @performers = Performer.all
   end
+  
+  def new
+  
+  end
+  
+  def show
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 Rails.application.routes.draw do
-
-  get 'opinions/terms'
-  get 'opinions/policy'
-  get 'opinions/transaction'
-
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions',
   }
   root to: 'homes#index'
+
   get 'performers/identification'
   get 'performers/full_body'
+  get 'opinions/terms'
+  get 'opinions/policy'
+  get 'opinions/transaction'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :homes, only: [:index]
+  resources :homes, only: [:index,:show,:new]
   resources :tests, only: [:index,:show]
   resources :performers, only: [:new, :show, :edit, :update]
   
