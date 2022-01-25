385d441ce9ed3f62efd27c7f8de9557d136dd1e1
rio <xr274375@fc5.so-net.ne.jp>
2022-01-25 11:27:31 +0900

    各曜日のスケジュールの時間を一覧に表示する機能を修正

--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -1,13 +1,22 @@
 module HomesHelper
   def today_schedule(performer)
     
-    case 0#Date.today.wday
+    case Date.today.wday
     when 0 then
       schedule(performer.sun_start.strftime('%H:%M'),performer.sun_end.strftime('%H:%M'))
     when 1 then
-      p "月"
+      p "本日 定休日"
     when 2 then
-      p "火"
+      schedule(performer.tue_start.strftime('%H:%M'),performer.tue_end.strftime('%H:%M'))
+    when 3 then
+      schedule(performer.wed_start.strftime('%H:%M'),performer.wed_end.strftime('%H:%M'))
+    when 4 then
+      schedule(performer.thu_start.strftime('%H:%M'),performer.thu_end.strftime('%H:%M'))
+    when 5 then
+      schedule(performer.fri_start.strftime('%H:%M'),performer.fri_end.strftime('%H:%M'))
+    when 6 then
+      schedule(performer.sat_start.strftime('%H:%M'),performer.sat_end.strftime('%H:%M'))
+
     else
       #いずれの値に一致しない時に実行されるとき
     end
