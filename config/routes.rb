8f204ae2749ed9d2d3af8e46d667be2cb6a33186
rio <xr274375@fc5.so-net.ne.jp>
2022-01-28 21:14:28 +0900

    求人情報のページを新規で追加

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,11 @@ Rails.application.routes.draw do
   get 'opinions/policy'
   get 'opinions/transaction'
   get 'opinions/service'
+  get 'homes/job_offer'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :homes, only: [:index,:show,:new]
-  resources :tests, only: [:index,:show]
+  resources :homes, only: [:index, :new, :show]
+  resources :tests, only: [:index, :show]
   resources :performers, only: [:new, :show, :edit, :update]
   
 end
