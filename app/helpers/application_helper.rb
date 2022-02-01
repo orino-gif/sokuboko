592a0a385327f724ca01b3e208c2e2969bb257a8
rio <xr274375@fc5.so-net.ne.jp>
2022-02-01 13:56:46 +0900

    サムネイル画像表示の為のメソッドを作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,22 @@
 module ApplicationHelper
+  
+  #サムネイル画像を表示。登録画像が無い場合は、代替え画像を表示。homes/indexの画像以外はリンク先を付与しない。
+  def display_thumbnail(performer)
+    if performer.image? 
+      if controller.controller_name == "homes"
+        link_to image_tag(performer.image.url, class: 'thumbnail'), "/performers/#{performer.user_id}"
+      else
+        image_tag(performer.image.url, class: 'thumbnail')
+      end
+    else
+      if controller.controller_name == "homes"
+        link_to image_tag("/img/pending.png", class: 'thumbnail'), "/performers/#{performer.user_id}"
+      else
+        image_tag("/img/pending.png", class: 'thumbnail')
+      end
+    end 
+  end
+  
   def schedule(start,finish)
     p start 
     if '00:00' != start && 'NULL' != start 
