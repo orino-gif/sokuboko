304616031469c1b19add92d20d6dc07e7aa83de7
rio <xr274375@fc5.so-net.ne.jp>
2022-02-02 14:55:05 +0900

    プレイ内容のレイアウトを変更

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,10 @@ Rails.application.routes.draw do
   get 'opinions/transaction'
   get 'opinions/service'
   get 'homes/job_offer'
+  get 'homes/play_content'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :homes, only: [:index, :new, :show]
+  resources :homes, only: [:index, :show]
   resources :tests, only: [:index, :show]
   resources :performers, only: [:new, :show, :edit, :update]
   resources :opinions, only: [:index]
