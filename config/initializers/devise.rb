b6890c2635596294c43d5c73eb33d4df1fd824c9
rio <xr274375@fc5.so-net.ne.jp>
2022-02-22 13:50:27 +0900

    リクエス機能を追加3

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -266,7 +266,11 @@ Devise.setup do |config|
   # config.navigational_formats = ['*/*', :html]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
-  config.sign_out_via = :delete
+  if Rails.env.production?
+    config.sign_out_via = :get
+  else
+    config.sign_out_via = :delete
+  end
 
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
